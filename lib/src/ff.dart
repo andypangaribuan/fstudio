498dfc2659827e34bdef67959ea93b2c7008dfea
Andy Pangaribuan <iam.pangaribuan@gmail.com>
2022-12-07 09:53:49 +0700

    ## 1.0.3 ### Fixed * typo on datetime parser for MMM format

--- lib/src/ff.dart
+++ lib/src/ff.dart
@@ -218,18 +218,18 @@ class _TimeUtil {
   bool _isCustomFormat = false;
 
   final _month3 = {
-    "jan": "01",
-    "feb": "02",
-    "mar": "03",
-    "apr": "04",
-    "may": "05",
-    "jun": "06",
-    "jul": "07",
-    "aug": "08",
-    "sep": "09",
-    "oct": "10",
-    "nov": "11",
-    "des": "12",
+    'jan': '01',
+    'feb': '02',
+    'mar': '03',
+    'apr': '04',
+    'may': '05',
+    'jun': '06',
+    'jul': '07',
+    'aug': '08',
+    'sep': '09',
+    'oct': '10',
+    'nov': '11',
+    'dec': '12',
   };
 
   DateTime? toTime(String? dt) {
